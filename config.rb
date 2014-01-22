9b501e3abd5646a16c6f2503de75be11669e2e99
Tiago Porto <tiagoporto@ymail.com>
2014-01-22 19:40:25 -0200

    Responsive Adds
    Media Queries
    -max 320px
    -max 480px
    -min 481px max 767px
    -min768px max 1139px
    -min 1140px

---
+++ config.rb
@@ -0,0 +1,12 @@
+css_dir = "/css"
+sass_dir = "/scss/style"
+images_dir = "/img"
+javascripts_dir = "/js"
+
+http_path = "/"
+http_images_path = "img"
+http_generated_images_path = "/img"
+relative_assets = true;
+
+output_style = :compressed
+line_comments = false
